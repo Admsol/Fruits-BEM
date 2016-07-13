8f9a407652b527c9b9b82a21209cb52f48be84f1
Pierre <pierre@moneypush.com>
2016-07-13 20:41:20 +0100

    Page and deletions

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,44 +1,55 @@
 module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-    haml: {
-      dist: {
-        files: {
-          // 1:1 compile
-          'index.html': 'index.haml',
+        haml: {
+          dist: {
+            files: {
+              // 1:1 compile
+              'temp/index.html': 'assets/views/src/index.haml',
+            }
+          }
+        },
+        less: {
+          dev: {
+            options: {
+              paths: ['assets/stylesheets/dist']
+            },
+            files: {
+              'assets/stylesheets/dist/app.css': 'assets/stylesheets/src/*'
+            },
+          },
+        },
+        watch: {
+          maintask: {
+            files: ['**/**.less', '**/**.haml'],
+            tasks: ['haml', 'less', 'htmlmin'],
+            options: {
+              livereload: true,
+            },
+          },
+        },
+        htmlmin: {
+          dist: {
+            options: {
+              removeComments: true,
+              collapseWhitespace: true
+            },
+            files: {
+              'index.html': 'temp/index.html',
+            },
+          },
         }
-      }
-    },
-    less: {
-      dev: {
-        options: {
-          paths: ['assets/stylesheets/dist']
-        },
-        files: {
-          'assets/stylesheets/dist/app.css': 'assets/stylesheets/src/*'
-        },
-      },
-    },
-    watch: {
-      maintask: {
-        files: 'assets/**.less',
-        files: '**/**.haml',
-        tasks: ['haml', 'less:dev'],
-        options: {
-          livereload: true,
-        },
-      },
-    },
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-haml2html');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+      grunt.loadNpmTasks('grunt-haml2html');
+      grunt.loadNpmTasks('grunt-contrib-less');
+      grunt.loadNpmTasks('grunt-contrib-watch');
+      grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
-  // Default task(s).
-  grunt.registerTask('default', ['watch']);
+      // Default task(s).
+      grunt.registerTask('default', ['watch']);
 
-};
+    };
